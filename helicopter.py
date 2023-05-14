442ab4ecdd9de458dd58d951ceba456ee5a75f3d
Andriy Smyk <105750583+smykandriy@users.noreply.github.com>
2023-05-14 18:24:48 +0300

    deleted main

--- helicopter.py
+++ helicopter.py
@@ -206,15 +206,3 @@ class Helicopter:
             self.__current_fuel = self.__fuel_capacity
             return
         self.__current_fuel += fuel
-
-
-if __name__ == "__main__":
-    helicopters = [
-        Helicopter(),
-        Helicopter(103, "Apache", 1200, 3000, 500, 430),
-        Helicopter.get_instance(),
-        Helicopter.get_instance(),
-    ]
-
-    for _ in helicopters:
-        print(_)
